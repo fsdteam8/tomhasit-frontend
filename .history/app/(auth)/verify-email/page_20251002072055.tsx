e15f639fccc09f56ed1da06c61b0f3730d7d4df5
badhon252 <khalidhossainbadhon@gmail.com>
2025-10-02 07:21:34 +0600

    feat(auth): implement change password and verify email functionality
    - Added ChangePasswordPage component for users to change their passwords.
    - Implemented password validation logic to ensure strong passwords.
    - Integrated API call to reset password with error handling and success feedback.
    - Created VerifyEmailPage component to handle OTP verification for email.
    - Added functionality to resend OTP with a countdown timer.
    - Improved user experience with loading states and error messages.
    - Updated package-lock.json with new dependencies for swc.

--- .history/app/(auth)/verify-email/page_20251002072055.tsx
+++ .history/app/(auth)/verify-email/page_20251002072055.tsx
@@ -1,10 +1,9 @@
-import VerifyEmailPage from '@/components/dashboard/auth/verify-email'
 import React, { Suspense } from 'react'
 
 export default function page() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <VerifyEmailPage />
+      <verify />
     </Suspense>
   )
 }
